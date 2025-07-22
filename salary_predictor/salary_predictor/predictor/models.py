f9cf8d6013d6d358c8c6314f7c048ac43ca75e28
Charles Owiti <mikeycharlesm7@gmail.com>
2025-07-22 19:42:01 +0300

    add models

--- salary_predictor/salary_predictor/predictor/models.py
+++ salary_predictor/salary_predictor/predictor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Countries(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class JobTitles(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
